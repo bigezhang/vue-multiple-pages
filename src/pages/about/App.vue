c31d23b40f5a28bcb3ee09d0c214c4fd25e5fa26
bigezhang <894239925@qq.com>
2018-01-21 21:04:08 +0800

    pageflow update

---
+++ src/pages/about/App.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="page">
+    <div class="content">
+      page2
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/main.scss";
+.page {
+  max-width: 640px;
+  margin: 0 auto;
+  font-size: 20px;
+}
+.content {
+  width: 100%;
+  height: 4rem;
+  background-color: #e77;
+}
+</style>
